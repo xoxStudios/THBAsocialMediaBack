d94c84d26bb8b8dcea7d171f411d3bc4aff82e7a
BSGMG18 <maximilianglomm@googlemail.com>
2020-02-25 12:22:50 +0100

    uninstall packages

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,7 +2,7 @@ const { create, getAll } = require("../../controllers/users");
 const router = require("express").Router();
 
 router
-  .route("/users")
+  .route("/")
   .post(create)
   .get(getAll);
 
